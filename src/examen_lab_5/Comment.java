d09587d2f4ae5e3bc16a42b1852a22dd8e2f388f
William A. Martinez Martinez <william_martinez@unitec.edu>
2025-02-21 13:27:14 -0600

    Clase comment lista

---
+++ src/examen_lab_5/Comment.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen_lab_5;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author DELL
+ */
+public class Comment {
+    private int postId;
+    private String autor;
+    private String comentario;
+    private Calendar fecha;
+
+    public Comment (int id, String autor, String comentario){
+        this.postId=id;
+        this.autor=autor;
+        this.comentario=comentario;
+        fecha=Calendar.getInstance();
+    }
+
+    public int getPostID() {
+        return postId;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public Calendar getFecha() {
+        return fecha;
+    }
+
+    public String print(){
+        return autor+ " - " + fecha +"\n" + comentario;
+    }
+}
