ff8422ff64ea656df93683a95c71e9c4b36134a8
William A. Martinez Martinez <william_martinez@unitec.edu>
2025-02-21 14:09:51 -0600

    Social class terminada

--- src/examen_lab_5/SocialClass.java
+++ src/examen_lab_5/SocialClass.java
@@ -10,9 +10,10 @@ import java.util.ArrayList;
  *
  * @author DELL
  */
-public class SocialClass {
-    ArrayList <String> friends, posts;
-    String username;
+public abstract class SocialClass {
+    public ArrayList <String> friends, posts;
+    public String username;
+    public String info;
 
     public SocialClass(String username) {
         this.friends = new ArrayList<>();
@@ -21,18 +22,40 @@ public class SocialClass {
     }
     
     public boolean addFriend(String user){
-        
+        if(!user.equals(username) && !friends.contains(user)){
+            friends.add(user);
+            return true;
+        }
+        return false;
     }
     
     public void addPost(String msg){
-        
+        posts.add(msg);
     }
     
-    public void timeline(){
-        
-    }
+    public abstract void timeline();
     
     public void myProfile(){
-        
+        info="User: "+username+"\n Timeline: \n";
+        timeline();
+        info+="\n Amigos: "+mostrarAmigos();
+    }
+    
+    public String mostrarAmigos(){
+        String amigos="";
+        for (int contar = 0; contar < friends.size(); contar++) {
+            if (friends.get(contar) != null && contar<10) {
+                amigos += "\n- " + friends.get(contar);
+            }
+        }
+        if (amigos.equals("")) {
+            return "No tiene amigos agregados";
+        }
+
+        return amigos;
+    }
+    
+    public String getProfile(){
+        return info;
     }
 }
