cc556209218c17ad99e687bfec69a01b37ca8963
Mario Enrique  Vasquez Adriano <marioenriquevasquez4@gmail.com>
2025-02-21 14:45:14 -0600

    Completando la logica de la clase Facebook

--- src/examen_lab_5/Facebook.java
+++ src/examen_lab_5/Facebook.java
@@ -4,10 +4,42 @@
  */
 package examen_lab_5;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Mario
  */
-public class Facebook {
-    
+public class Facebook extends SocialClass implements Commentable {
+
+    private ArrayList<Comment> comentarios;
+
+    public Facebook(String username) {
+        super(username);
+        comentarios = new ArrayList<>();
+    }
+
+    @Override
+    public void timeline() {
+        for (int i = 0; i < posts.size(); i++) {
+            info += "POST " + i + ":\n";
+            info += posts.get(i) + "\n";
+            for (Comment c : comentarios) {
+                if (c.getPostID() == i) {
+                    info += "   " + c.toString() + "\n";
+                }
+            }
+            info += "\n";
+        }
+    }
+
+    @Override
+    public boolean addComment(Comment comment) {
+        int postId = comment.getPostID();
+        if (postId < 0 || postId >= posts.size()) {
+            return false;
+        }
+        comentarios.add(comment);
+        return true;
+    }
 }
